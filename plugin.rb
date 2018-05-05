66eb6683a65fc9e505ceba5ea5b53e26ad787553
Dan Faulknor <danielfaulknor@users.noreply.github.com>
2018-05-05 17:33:06 +1200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -73,17 +73,19 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
   def fetch_user_details(token, id)
     user_json_url = SiteSetting.oauth2_user_json_url.sub(':token', token.to_s).sub(':id', id.to_s)
     user_json_method = SiteSetting.oauth2_user_json_url_method
+    
+    user_url = "#{user_json_url}?access_token=#{token}"
 
-    log("user_json_url: #{user_json_method} #{user_json_url}")
+    log("user_json_url: #{user_json_method} #{user_url}")
 
     bearer_token = "Bearer #{token}"
     user_json_response =
       if user_json_method.downcase.to_sym == :post
         Net::HTTP
-          .post_form(URI(user_json_url), { 'Authorization' => bearer_token })
+          .post_form(URI(user_url), { 'Authorization' => bearer_token })
           .body
       else
-        open(user_json_url, 'Authorization' => bearer_token).read
+        open(user_url, 'Authorization' => bearer_token).read
       end
 
     user_json = JSON.parse(user_json_response)
